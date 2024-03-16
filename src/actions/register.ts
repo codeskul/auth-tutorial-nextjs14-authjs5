09bb551bf58686378713b34fe2b64d929bb0cb07
sam new lap <sam@gmail.com>
2024-03-16 14:20:03 +0530

    Register Functionality

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,11 @@
 "use server"
 
-import { registerSchema } from "@/schemas"
 import *  as z from "zod"
+import bcrypt from "bcrypt"
+
+import { db } from "@/lib/db"
+import { registerSchema } from "@/schemas"
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
     const validatedFields = registerSchema.safeParse(values)
@@ -10,7 +14,26 @@ export const register = async (values: z.infer<typeof registerSchema>) => {
         return { error: "Invalid fields!" }
     }
 
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+        return { error: "Email already in use!" }
+    }
+
+    await db.user.create({
+        data: {
+            email,
+            name,
+            password: hashedPassword
+        }
+    })
+
+    // TODO: send verification token email
+
     return {
-        success: "Email sent!"
+        success: "User created!"
     }
 }
