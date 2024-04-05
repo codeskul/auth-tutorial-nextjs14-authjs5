15e124fa07ba4abcd08ede075df86ed7b0f9bfd9
sam new lap <sam@gmail.com>
2024-04-05 15:26:38 +0530

    ExtendedUser type error solve

--- src/components/user-info.tsx
+++ src/components/user-info.tsx
@@ -41,7 +41,6 @@ export const UserInfo = ({ user, label }: UserInfoProp) => {
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-medium">Two Factor Authentication</p>
           <Badge variant={user?.isTwoFactorEnabled ? "success" : "destructive"}>
-
             {user?.isTwoFactorEnabled ? "ON" : "OFF"}
           </Badge>
         </div>
--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -1,20 +1,26 @@
-import { UserRole } from "@prisma/client"
+import { UserRole } from "@prisma/client";
 
-import NextAuth, { type DefaultSession } from "next-auth"
+import NextAuth, { type DefaultSession } from "next-auth";
 declare module "next-auth" {
-    interface Session {
-        user: {
-            role: UserRole
-            isTwoFactorEnabled: boolean
-        } & DefaultSession["user"]
-    }
+  interface Session {
+    user: {
+      role: UserRole;
+      isTwoFactorEnabled: boolean;
+    } & DefaultSession["user"];
+  }
 }
 
-import { JWT } from "next-auth/jwt"
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+  isTwoFactorEnabled: boolean;
+};
+
+import { JWT } from "next-auth/jwt";
 declare module "next-auth/jwt" {
-    /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
-    interface JWT {
-        /** OpenID ID Token */
-        role?: UserRole
-    }
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    role?: UserRole;
+    isTwoFactorEnabled?: boolean;
+  }
 }
